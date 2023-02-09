2741f10a7ccdb927def4f90a825371940285f10e
JuanJe <juanje.llamas@gmail.com>
2023-02-10 00:13:13 +0100

    Mas iteraciones

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
 
-const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+const MONGODB_URI = "mongodb://127.0.0.1/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then((x) => {
+
+  .then((response)=>{
+
+return Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration: 100} )
+
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });
+
+
+ /* .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
@@ -36,12 +47,11 @@ mongoose
         image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
         duration: 40,
         creator: "Chef LePapu"
+
       }
      );
   })
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database", error);
-  });
+  .then((response)=>{
+Recipe.insertMany(data)
+
+  })   */
